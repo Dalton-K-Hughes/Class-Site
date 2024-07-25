3a4b45b299cc9d6bffe65861fad81e9dcb496d41
Dalton-K-Hughes <dhughes3223@gmail.com>
2024-07-25 16:12:07 -0400

    Added  base routes to app.py for other each classes site, tweaked some css, added new dependencies to requirements.txt

--- app.py
+++ app.py
@@ -5,3 +5,24 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/intro-business')
+def intro_business():
+    pass
+
+@app.route('/foundations-interactive')
+def foundations_interactive():
+    pass
+
+@app.route('/foundations-investing')
+def foundations_investing():
+    pass
+
+@app.route('intro-software')
+def intro_software():
+    pass
+
+@app.route('web-design')
+def web_design():
+    pass
+
